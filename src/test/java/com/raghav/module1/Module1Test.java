6c0bd5c8c85e1b6dde25d516b4858d38e0590502
Raghavendra Vaidya <rkvaidya58@gmail.com>
2025-03-25 12:40:00 +0530

    update test cases

--- src/test/java/com/raghav/module1/Module1Test.java
+++ src/test/java/com/raghav/module1/Module1Test.java
@@ -1,21 +1,23 @@
 package com.raghav.module1;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Module1Test {
 
     @Test
     public void testEnv() {
-
+        System.out.println("Running testEnv test");
+        Assert.assertTrue(true);
     }
 
     @Test
     public void test1() {
-
+        Assert.assertTrue(true);
     }
 
     @Test
     public void test2() {
-
+        Assert.assertTrue(true);
     }
 }
--- src/test/java/com/raghav/module2/Module2Test.java
+++ src/test/java/com/raghav/module2/Module2Test.java
@@ -1,5 +1,6 @@
 package com.raghav.module2;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Module2Test {
@@ -7,11 +8,11 @@ public class Module2Test {
 
     @Test
     public void test3() {
-
+        Assert.assertTrue(true);
     }
 
     @Test
     public void test4() {
-
+        Assert.fail();
     }
 }
